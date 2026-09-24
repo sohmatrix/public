70248f75721ae974b54f13d4229e5abe996f7543
sohma <hidetomo@sohma.com>
2026-09-24 23:30:48 +0900

    delete all files at ".pio" and some files at ".vscode" in sample program 001 and 002.

--- samplePrograms/LILYGO_T_Display_S3/Projects/002_helloWorld_usingDislpay/.pio/build/lilygo-t-display-s3/FrameworkArduino/esp32-hal-ldo.c.d
+++
@@ -1,8 +0,0 @@
-.pio/build/lilygo-t-display-s3/FrameworkArduino/esp32-hal-ldo.c.o: \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-ldo.c \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-ldo.h \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/qio_opi/include/sdkconfig.h \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/soc/esp32s3/include/soc/soc_caps.h \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_err.h \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/newlib/platform_include/stdio.h \
- /Users/sohma/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_compiler.h
