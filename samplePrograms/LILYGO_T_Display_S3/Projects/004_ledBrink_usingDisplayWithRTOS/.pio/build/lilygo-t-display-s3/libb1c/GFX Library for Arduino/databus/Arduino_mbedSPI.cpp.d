e9f7a1914a694ad69ab7a81da0e12720508e09d3
sohma <hidetomo@sohma.com>
2026-09-24 17:47:07 +0900

    delete test .pio

--- samplePrograms/LILYGO_T_Display_S3/Projects/004_ledBrink_usingDisplayWithRTOS/.pio/build/lilygo-t-display-s3/libb1c/GFX Library for Arduino/databus/Arduino_mbedSPI.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/lilygo-t-display-s3/libb1c/GFX\ Library\ for\ Arduino/databus/Arduino_mbedSPI.cpp.o: \
- .pio/libdeps/lilygo-t-display-s3/GFX\ Library\ for\ Arduino/src/databus/Arduino_mbedSPI.cpp
